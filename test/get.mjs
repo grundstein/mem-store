54713754362a41d7470454bb03fed3925112372f
jaeh <jascha@jaeh.at>
2021-09-23 22:58:13 +0200

    update test to make sure we do not throw by accident

--- test/get.mjs
+++ test/get.mjs
@@ -1,13 +1,17 @@
+import { is } from '@magic/test'
+
 import memStore from '../src/index.mjs'
 
-const store = memStore({ data: 'is set' })
+const data = { key: 'is set' }
+
+const store = memStore(data)
 
 export default [
-  { fn: () => store.get('data'), expect: 'is set', info: 'expect data to be gettable' },
+  { fn: () => store.get('key'), expect: 'is set', info: 'expect data to be gettable' },
   {
     fn: () => store.get({ data: 'value' }),
     expect: undefined,
-    info: 'no to error if key is weird',
+    info: 'no error if key is weird',
   },
   {
     fn: () => store.get(new Date()),
@@ -18,5 +22,9 @@ export default [
     fn: () => store.get(new Error()),
     expect: undefined,
     info: 'no error if key is Error',
+  },
+  {
+    fn: () => store.get(false),
+    expect: is.deep.equal(data),
   },
 ]
--- test/set.mjs
+++ test/set.mjs
@@ -1,4 +1,4 @@
-import { is, tryCatch } from '@magic/test'
+import { tryCatch } from '@magic/test'
 
 import memStore from '../src/index.mjs'
 
@@ -12,18 +12,34 @@ export default [
     info: 'data is settable via number and then gettable via string',
   },
   {
-    fn: tryCatch(store.set, ''),
+    fn: tryCatch(store.set, '', true),
     expect: t => t.code === 'E_KEY_EMPTY',
-    info: 'empty key errors with E_KEY_EMPTY',
+    info: 'empty key throws errors if second arg is true with E_KEY_EMPTY',
+  },
+  {
+    fn: store.set(''),
+    expect: t => t.code === 'E_KEY_EMPTY',
+    info: 'empty key returns error with code === E_KEY_EMPTY',
   },
   {
     fn: tryCatch(store.set, [235], true),
     expect: t => t.code === 'E_KEY_TYPE',
-    info: 'array as key errors with E_KEY_TYPE',
+    info: 'array as key throws errors with E_KEY_TYPE if second arg is true',
+  },
+  {
+    fn: store.set([235]),
+    expect: t => t.code === 'E_KEY_TYPE',
+    info: 'array as key returns errors with E_KEY_TYPE',
   },
   {
     fn: tryCatch(store.set, { test: 235 }, true),
     expect: t => t.code === 'E_KEY_TYPE',
     info: 'object as key errors with E_KEY_TYPE',
+  },
+
+  {
+    fn: store.set({ test: 235 }),
+    expect: t => t.code === 'E_KEY_TYPE',
+    info: 'array as key returns errors with E_KEY_TYPE',
   },
 ]
