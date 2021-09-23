9b225ca1b844e84302f0bf3c8e599bca26e6552d
jaeh <jascha@jaeh.at>
2021-09-23 22:57:42 +0200

    src/index: return errors by default, allow throwing errors if second constructor argument is true

--- src/index.mjs
+++ src/index.mjs
@@ -3,14 +3,22 @@ import is from '@magic/types'
 
 const libName = '@grundstein/mem-store'
 
-export const memStore = (data = {}) => ({
+export const memStore = (data = {}, throwError = false) => ({
   set: (key, val) => {
     if (is.empty(key)) {
-      throw error(`${libName}.set: key must be non-empty`, 'KEY_EMPTY')
+      const err = error(`${libName}.set: key must be non-empty`, 'KEY_EMPTY')
+      if (throwError) {
+        throw err
+      }
+      return err
     }
 
     if (!is.string(key) && !is.number(key)) {
-      throw error(`${libName}.set: key must be a string or a number, got ${typeof key}`, 'KEY_TYPE')
+      const err = error(`${libName}.set: key must be a string or a number, got ${typeof key}`, 'KEY_TYPE')
+      if (throwError) {
+        throw err
+      }
+      return err
     }
 
     data[key] = val
@@ -18,9 +26,17 @@ export const memStore = (data = {}) => ({
 
   // only return full cache if no key was passed to function, not if key is empty, null, undefined.
   // this SHOULD prevent get from erroring at runtime.
-  get: (key = false) => (key !== false ? data[key] : data),
+  get: (key = false) => {
+    if (data.hasOwnProperty(key)) {
+      return data[key]
+    }
 
-  reset: key => {
+    if (key === false) {
+      return data
+    }
+  },
+
+  reset: () => {
     data = {}
   },
 })
